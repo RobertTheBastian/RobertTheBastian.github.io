9c7b45122752153db62b4369bab1a3d49ea8d3b3
RobertTheBastian <53890016+RobertTheBastian@users.noreply.github.com>
2020-03-29 18:36:45 +0100

    Laptop 29/03

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
-$(document).ready(function() {
+//profile scroll effect
+$(document).ready(function () {
   var $profile = $('.profile');
-  var docWidth = $( document).width();
+  var docWidth = $(document).width();
   var speed = 22;
   var startSize = 196;
-  var startPosition = (docWidth /2) - (startSize /2);
+  var startPosition = (docWidth / 2) - (startSize / 2);
 
   $profile.css({
-    'left':startPosition,
+    'left': startPosition,
     'width': startSize,
     'height': startSize
+  });
+
+  $(window).scroll(function () {
+
+    var st = $(this).scrollTop();
+    var newPos = startPosition - (st * (speed / 6));
+    var newSize = startSize - (st * (speed / 100));
+
+    $profile.css({
+      'left': newPos,
+      'width': newSize,
+      'height': newSize
+    });
+  });
 });
 
-  $(window).scroll(function() {
-
-      var st = $(this).scrollTop();
-      var newPos = startPosition - (st * (speed / 6));
-      var newSize = startSize - (st * (speed/100));
-
-      $profile.css({
-        'left': newPos,
-        'width': newSize,
-        'height': newSize
-      });
-
+//Hero particle BG
+$(document).ready(function () {
+  $('#particles').particleground({
+    dotColor: '#ff3358',
+    lineColor: '#bf7c89',
+    maxSpeedX: 0.9,
+    maxSpeedY: 0.9,
 
   });
 });
